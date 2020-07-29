71ed065288fa527eebc14386ef1efc2baf231fdb
Lakshmijvl <68075536+Lakshmijvl@users.noreply.github.com>
2020-07-29 19:56:39 -0400

    Update SignUpPage.java

--- src/main/pages/SignUpPage.java
+++ src/main/pages/SignUpPage.java
@@ -1,16 +1,60 @@
-public class SignUpPage {
+public class SignUpPage  {
 	
 	private Selenium selenium;
 	
 	// Add XPaths here
-	private static final String firstNameInput = "/html/body/div[1]/div/div/div/div[2]/div[2]/form/div[1]/div/div[1]/input";
-	
+	By setfirstname = By.xpath("//input[@id='user_first_name']");
+	By setlastname = By.xpath("//input[@id='user_last_name']");
+	By setEmail = By.xpath("//input[@id='user_email']");
+	By setpassword = By.xpath("//input[@id='user_password']");
+	By clicksubmit =By.xpath("//button[@id='btn_register']");
 	
 	public SignUpPage(){
 		this.selenium = Selenium();
 	}
+
 	
 	// Create a function that takes an Account and simulates signing up using Selenium actions. (Refer to Selenium.java)
+	
+	//Enter First Name
+	
+	public void firstname(String text) 
+	{
+		selenium.sendKeys(setfirstname, text);
+	
+
+	}
+
+	// Enter LastName
+	public void lastname(String text)
+
+	{
+		selenium.sendKeys(setlastname, text);
+	}
+
+	// Enter Email
+	public void email(String text)
+
+	{
+		selenium.sendKeys(setEmail, text);
+
+	}
+	
+	//Enter Password
+	public void password(String text)
+	{
+	 
+		selenium.sendKeys(setpassword, text);
+		
+	}
+	
+	//click submit
+	
+	
+	public void submit()
+	{
+		selenium.click(clicksubmit);
+	
 	
 	
 }
