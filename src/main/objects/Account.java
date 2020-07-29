190de2e8d181601ab9fe1054911b3dfd677f36c5
Lakshmijvl <68075536+Lakshmijvl@users.noreply.github.com>
2020-07-29 19:56:29 -0400

    Update Account.java

--- src/main/objects/Account.java
+++ src/main/objects/Account.java
@@ -7,8 +7,36 @@ public class Account {
 	* Email
 	* Password
 	**/
+	public static final String FirstName = "QA";
+	public static final String Lastname = "Test";
+	public static final String Email = "qa.techtest@venngage.com";
+	public static final String Password = "123456";
 	
+		
+	}
 	
+	public String FirstName()
+	{
+		return FirstName;
+	}
+	
+	public String Lastname()
+	{
+		return Lastname;
+	}
+	public String Email()
+	{
+		return Email;
+	}
+	public String Password()
+	{
+		return Password;
+	}
 	
 	// Create the necessary Getter functions
+	
+	
+	
+	
+
 }
